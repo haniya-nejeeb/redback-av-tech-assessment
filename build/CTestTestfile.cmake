d66ef30954dd8fd8058e1c520cf254e639275163
Haniya Nejeeb <z5520747@ad.unsw.edu.au>
2025-03-11 12:26:16 +1100

    Set up cmake and build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment
+# Build directory: /mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(question-1 "Question-1-out")
+set_tests_properties(question-1 PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;13;add_test;/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;0;")
+add_test(question-2 "Question-2-out")
+set_tests_properties(question-2 PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;13;add_test;/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;0;")
+add_test(question-3 "Question-3-out")
+set_tests_properties(question-3 PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;13;add_test;/mnt/c/Users/haniy/OneDrive/desktop/uni/2025/redback av tech assessment/CMakeLists.txt;0;")
